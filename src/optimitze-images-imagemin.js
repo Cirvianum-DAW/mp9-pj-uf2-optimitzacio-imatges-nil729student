473f57c0d13f213fe1ba0f00aabb12b37c89dc13
your_username <you@example.com>
2023-12-20 14:08:54 +0100

    clip-path

--- src/optimitze-images-imagemin.js
+++ src/optimitze-images-imagemin.js
@@ -1,31 +1,41 @@
-// Replace these lines
-// const imagemin = require('imagemin');
-// const imageminPngquant = require('imagemin-pngquant');
-// const imageminJpegtran = require('imagemin-jpegtran');
-// const imageminGifsicle = require('imagemin-gifsicle');
-// const imageminSvgo = require('imagemin-svgo');
-
-// With these lines
+// Importem totes les llibreries que necessitem 
 import imagemin from 'imagemin';
+// Hi ha llibreria per cada tipus d'imatge
 import imageminPngquant from 'imagemin-pngquant';
 import imageminJpegtran from 'imagemin-jpegtran';
+import imageminWebp from 'imagemin-webp';
 import imageminGifsicle from 'imagemin-gifsicle';
 import imageminSvgo from 'imagemin-svgo';
 
-// Continue with the rest of your code as is
-// ...
-
-// Example:
+// Executem la funció
 (async () => {
-  await imagemin(['test/img/*.{jpg,png,gif,svg}'], {
-    destination: 'test/dist/',
+  // Optimitzar JPG
+  await imagemin(['test/img/*.jpg'], {
+    destination: 'test/dist', // Carpeta destí
     plugins: [
-      imageminJpegtran(),
-      imageminPngquant({ quality: [0.6, 0.8] }),
-      imageminGifsicle(),
-      imageminSvgo(),
+      // Optimitza JPG significa que es carrega de mica en mica i la qualitat de la imatge 50 
+    imageminJpegtran({ progressive: true, quality: 50 }), 
     ],
   });
 
-  console.log('Images optimized');
+  // Optimitzar PNG
+  await imagemin(['test/img/*.png'], {
+    destination: 'test/dist/',
+    plugins: [
+      imageminPngquant({
+        quality: [0.4, 0.6],// Qualitat de la imatge entre 0.4 i 0.6 respecte la qualitat original
+        speed: 1, // Velocitat de compressió
+      }),
+    ],
+  });
+
+  // Convertir imágenes a formato WebP
+  await imagemin(['test/img/*.{jpg,png}'], {
+    destination: 'test/dist/',
+    plugins: [
+      imageminWebp({ quality: 50 }), 
+    ],
+  });
+
+  console.log('Imágenes optimizadas');
 })();
